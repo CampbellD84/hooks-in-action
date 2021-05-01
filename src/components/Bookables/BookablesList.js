77408720b4f9f5dc5e1390b265a7b41198d2969e
Douglas Campbell <dacampbelltbone@gmail.com>
2021-05-01 00:01:51 -0400

    set Bookable index with useState hook.

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,20 +1,16 @@
+import { useState } from "react";
 import { bookables } from "../../static.json";
 
 export default function BookablesList() {
   const group = "Rooms";
   const bookablesInGroup = bookables.filter((b) => b.group === group);
-  let bookableIndex = 1;
-
-  function changeBookable(selectedIndex) {
-    bookableIndex = selectedIndex;
-    console.log(selectedIndex);
-  }
+  const [bookableIndex, setBookableIndex] = useState(1);
 
   return (
     <ul className="bookables items-list-nav">
       {bookablesInGroup.map((b, i) => (
         <li key={b.id} className={i === bookableIndex ? "selected" : null}>
-          <button className="btn" onClick={() => changeBookable(i)}>
+          <button className="btn" onClick={() => setBookableIndex(i)}>
             {b.title}
           </button>
         </li>
