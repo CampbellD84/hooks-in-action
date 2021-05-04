5a207c0ce9fb41768376ec7f888d4546a5ea0311
Douglas Campbell <dacampbelltbone@gmail.com>
2021-05-04 00:58:50 -0400

    Complete last challenge of chapter 2, add details of users in userslist component.

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -3,16 +3,31 @@ import { users } from "../../static.json";
 
 export default function UsersList() {
   const [usersIndex, setUsersIndex] = useState(0);
+  const user = users[usersIndex];
 
   return (
-    <ul className="bookables items-list-nav">
-      {users.map((user, i) => (
-        <li key={user.id} className={i === usersIndex ? "selected" : null}>
-          <button className="btn" onClick={() => setUsersIndex(i)}>
-            {user.name}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="bookables items-list-nav">
+        {users.map((user, i) => (
+          <li key={user.id} className={i === usersIndex ? "selected" : null}>
+            <button className="btn" onClick={() => setUsersIndex(i)}>
+              {user.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      {user && (
+        <div className="item user">
+          <div className="item-header">
+            <h2>{user.name}</h2>
+          </div>
+          <div className="user-details">
+            <h3>{user.title}</h3>
+            <p>{user.notes}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
