a22e9d7182847a480efa4992d8b3b0079e70b876
Douglas Campbell <dacampbelltbone@gmail.com>
2021-05-05 21:01:42 -0400

    Implement useReducer hook to handle complex state. Create reducer function to import into BookablesList component.

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,22 +1,45 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import { bookables, sessions, days } from "../../static.json";
 import { FaArrowRight } from "react-icons/fa";
 
-export default function BookablesList() {
-  const [group, setGroup] = useState("Kit");
-  const bookablesInGroup = bookables.filter((b) => b.group === group);
-  const [bookableIndex, setBookableIndex] = useState(0);
-  const groups = [...new Set(bookables.map((b) => b.group))];
-  const bookable = bookablesInGroup[bookableIndex];
-  const [hasDetails, setHasDetails] = useState(false);
+import reducer from "./reducer";
 
-  function changeGroup(event) {
-    setGroup(event.target.value);
-    setBookableIndex(0);
+const initialState = {
+  group: "Rooms",
+  bookableIndex: 0,
+  hasDetails: true,
+  bookables,
+};
+
+export default function BookablesList() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { group, bookableIndex, bookables, hasDetails } = state;
+
+  const bookablesInGroup = bookables.filter((b) => b.group === group);
+  const bookable = bookablesInGroup[bookableIndex];
+  const groups = [...new Set(bookables.map((b) => b.group))];
+
+  function changeGroup(e) {
+    dispatch({
+      type: "SET_GROUP",
+      payload: e.target.value,
+    });
+  }
+
+  function changeBookable(selectedIndex) {
+    dispatch({
+      type: "SET_BOOKABLE",
+      payload: selectedIndex,
+    });
   }
 
   function nextBookable() {
-    setBookableIndex((i) => (i + 1) % bookablesInGroup.length);
+    dispatch({ type: "NEXT_BOOKABLE" });
+  }
+
+  function toggleDetails() {
+    dispatch({ type: "TOGGLE_HAS_DETAILS" });
   }
 
   return (
@@ -32,7 +55,7 @@ export default function BookablesList() {
         <ul className="bookables items-list-nav">
           {bookablesInGroup.map((b, i) => (
             <li key={b.id} className={i === bookableIndex ? "selected" : null}>
-              <button className="btn" onClick={() => setBookableIndex(i)}>
+              <button className="btn" onClick={() => changeBookable(i)}>
                 {b.title}
               </button>
             </li>
@@ -55,7 +78,7 @@ export default function BookablesList() {
                   <input
                     type="checkbox"
                     checked={hasDetails}
-                    onChange={() => setHasDetails((has) => !has)}
+                    onChange={toggleDetails}
                   />
                   Show Details
                 </label>
