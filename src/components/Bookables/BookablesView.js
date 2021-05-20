4c167b032a63418e2abee60b707e24d847e274f4
Douglas Campbell <dacampbelltbone@gmail.com>
2021-05-20 00:40:34 -0400

    Sharing state value and updater function from useState. Lifting state that only needs to be shared.

--- src/components/Bookables/BookablesView.js
+++ src/components/Bookables/BookablesView.js
@@ -1,29 +1,14 @@
-import { useReducer } from "react";
+import { useState } from "react";
 
 import BookablesList from "./BookablesList";
 import BookableDetails from "./BookableDetails";
 
-import reducer from "./reducer";
-
-const initialState = {
-  group: "Rooms",
-  bookableIndex: 0,
-  bookables: [],
-  isLoading: true,
-  error: false,
-};
-
 export default function BookablesView() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const bookablesInGroup = state.bookables.filter(
-    (b) => b.group === state.group
-  );
-  const bookable = bookablesInGroup[state.bookableIndex];
+  const [bookable, setBookable] = useState();
 
   return (
     <>
-      <BookablesList state={state} dispatch={dispatch} />
+      <BookablesList bookable={bookable} setBookable={setBookable} />
       <BookableDetails bookable={bookable} />
     </>
   );
