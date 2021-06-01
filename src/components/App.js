2533f656457ff7904562e2a2ca0d045a05cdaa9d
Douglas Campbell <dacampbelltbone@gmail.com>
2021-05-31 23:51:47 -0400

    Add Edit button to Booking Details when user is the same as the booker. Add UserContext for user to pass down to other components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "../App.css";
 
@@ -8,42 +9,48 @@ import BookablesPage from "./Bookables/BookablesPage";
 import UsersPage from "./Users/UsersPage";
 import UserPicker from "./Users/UserPicker";
 
-export default function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/bookings" className="btn btn-header">
-                  <FaCalendarAlt />
-                  <span>Bookings</span>
-                </Link>
-              </li>
-              <li>
-                <Link to="/bookables" className="btn btn-header">
-                  <FaDoorOpen />
-                  <span>Bookables</span>
-                </Link>
-              </li>
-              <li>
-                <Link to="/users" className="btn btn-header">
-                  <FaUsers />
-                  <span>Users</span>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <UserPicker />
-        </header>
+import UserContext from "./Users/UserContext";
 
-        <Routes>
-          <Route path="/bookings" element={<BookingsPage />} />
-          <Route path="/bookables" element={<BookablesPage />} />
-          <Route path="/users" element={<UsersPage />} />
-        </Routes>
-      </div>
-    </Router>
+export default function App() {
+  const [user, setUser] = useState();
+
+  return (
+    <UserContext.Provider value={user}>
+      <Router>
+        <div className="App">
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/bookings" className="btn btn-header">
+                    <FaCalendarAlt />
+                    <span>Bookings</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/bookables" className="btn btn-header">
+                    <FaDoorOpen />
+                    <span>Bookables</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/users" className="btn btn-header">
+                    <FaUsers />
+                    <span>Users</span>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <UserPicker user={user} setUser={setUser} />
+          </header>
+
+          <Routes>
+            <Route path="/bookings" element={<BookingsPage />} />
+            <Route path="/bookables" element={<BookablesPage />} />
+            <Route path="/users" element={<UsersPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </UserContext.Provider>
   );
 }
