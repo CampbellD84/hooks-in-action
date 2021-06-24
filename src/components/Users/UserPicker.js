2af77b6e2628daa83c7518fc1e267374603da9b6
Douglas Campbell <dacampbelltbone@gmail.com>
2021-06-24 01:11:09 -0400

    Create useFetch custom hook and use in BookablesList, UserPicker, and UsersList components.

--- src/components/Users/UserPicker.js
+++ src/components/Users/UserPicker.js
@@ -1,19 +1,15 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useUser } from "./UserContext";
 import Spinner from "../UI/Spinner";
+import useFetch from "../../utils/useFetch";
 
 export default function UserPicker() {
   const [user, setUser] = useUser();
-  const [users, setUsers] = useState(null);
+  const { data: users = [], status } = useFetch("http://localhost:3001/users");
 
   useEffect(() => {
-    fetch("http://localhost:3001/users")
-      .then((resp) => resp.json())
-      .then((data) => {
-        setUsers(data);
-        setUser(data[0]);
-      });
-  }, [setUser]);
+    setUser(users[0]);
+  }, [users, setUser]);
 
   function handleSelect(e) {
     const selectedID = parseInt(e.target.value, 10);
@@ -22,8 +18,12 @@ export default function UserPicker() {
     setUser(selectedUser);
   }
 
-  if (users === null) {
+  if (status === "loading") {
     return <Spinner />;
+  }
+
+  if (status === "error") {
+    return <span>Error!</span>;
   }
 
   return (
